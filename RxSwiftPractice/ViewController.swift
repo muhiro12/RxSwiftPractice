51d24b41c6492fa465c9457553f7cbe292dc749e
Hiromu Nakano <hiromu.nakano.12@gmail.com>
2020-03-22 23:14:53 +0900

    Layout views

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondText: UILabel!
+    @IBOutlet weak var secondButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        firstText.delegate = self
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
